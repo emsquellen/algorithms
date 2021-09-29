3f68475220819ba7a76841b9175c9903481a82a7
emsquellen <24551188+emsquellen@users.noreply.github.com>
2021-09-29 16:07:26 +0200

    MergeSort

--- Sorting.java
+++ Sorting.java
@@ -77,21 +77,85 @@ public class Sorting {
         }
     }
 
+    public static void mergeSort(int[] someArray) {
+        int left = 0;
+        int right = someArray.length - 1;
+        recursiveMergeSort(someArray, left, right);
+    }
+
+    private static void recursiveMergeSort(int[] someArray, int left, int right) {
+        if (right > left) {
+            int middle = left + (right - left) / 2;
+            recursiveMergeSort(someArray, left, middle);
+            recursiveMergeSort(someArray, middle + 1, right);
+            merge(someArray, left, middle, right);
+        }
+    }
+
+    private static void merge(int[] someArray, int left, int middle, int right) {
+        int leftSize = middle - left + 1;
+        int rightSize = right - middle;
+    
+        int[] leftArray = new int[leftSize];
+        int[] rightArray = new int[rightSize];
+    
+        for (int i = 0; i < leftSize; i++){
+            leftArray[i] = someArray[left + i];
+        }
+        for (int j = 0; j < rightSize; j++){
+            rightArray[j] = someArray[middle + 1 + j];
+        }
+    
+        int i = 0;
+        int j = 0;
+        int k = left;
+    
+        while ((i < leftSize) && (j < rightSize)){
+            if (leftArray[i] <= rightArray[j]){
+                someArray[k] = leftArray[i];
+                i++;
+            } else {
+                someArray[k] = rightArray[j];
+                j++;
+            } k++;
+        }
+        while (i < leftSize) {
+            someArray[k] = leftArray[i];
+            i++;
+            k++;
+        }
+        while (j < rightSize) {
+            someArray[k] = rightArray[j];
+            j++;
+            k++;
+        }
+    }
+
     public static void main(String[] args) {
-        int[] nums = { 48, 8, 78, 67, 7, 79, 21, 97, 29, 36, 19, 5, 63, 64, 10, 1, 20, 77, 28, 12, 98, 93, 54, 26, 37, 73, 60, 88, 96, 82, 35, 2, 86, 66, 46, 9, 52, 45, 87, 22, 44, 25, 75, 4, 3, 69, 14, 32, 68, 92, 7, 79, 21, 97, 29, 36, 19, 5, 63, 64, 10, 1, 20 };
+        int[] nums = { 48, 8, 78, 67, 7, 79, 21, 97, 29, 36, 19, 5, 63, 64, 10, 1, 20, 77, 28, 12, 98, 93, 54, 26, 37,
+                73, 60, 88, 96, 82, 35, 2, 86, 66, 46, 9, 52, 45, 87, 22, 44, 25, 75, 4, 3, 69, 14, 32, 68, 92, 7, 79,
+                21, 97, 29, 36, 19, 5, 63, 64, 10, 1, 20 };
         insertionSort(nums);
         System.out.println(Arrays.toString(nums));
-        int[] nums2 = { 48, 8, 78, 67, 7, 79, 21, 97, 29, 36, 19, 5, 63, 64, 10, 1, 20, 77, 28, 12, 98, 93, 54, 26, 37, 73, 60, 88, 96, 82, 35, 2, 86, 66, 46, 9, 52, 45, 87, 22, 44, 25, 75, 4, 3, 69, 14, 32, 68, 92, 7, 79, 21, 97, 29, 36, 19, 5, 63, 64, 10, 1, 20 };
+        int[] nums2 = { 48, 8, 78, 67, 7, 79, 21, 97, 29, 36, 19, 5, 63, 64, 10, 1, 20, 77, 28, 12, 98, 93, 54, 26, 37,
+                73, 60, 88, 96, 82, 35, 2, 86, 66, 46, 9, 52, 45, 87, 22, 44, 25, 75, 4, 3, 69, 14, 32, 68, 92, 7, 79,
+                21, 97, 29, 36, 19, 5, 63, 64, 10, 1, 20 };
         selectionSort(nums2);
         System.out.println(Arrays.toString(nums2));
-        int[] nums3 = { 48, 8, 78, 67, 7, 79, 21, 97, 29, 36, 19, 5, 63, 64, 10, 1, 20, 77, 28, 12, 98, 93, 54, 26, 37, 73, 60, 88, 96, 82, 35, 2, 86, 66, 46, 9, 52, 45, 87, 22, 44, 25, 75, 4, 3, 69, 14, 32, 68, 92, 7, 79, 21, 97, 29, 36, 19, 5, 63, 64, 10, 1, 20 };
-        selectionSort(nums3);
+        int[] nums3 = { 48, 8, 78, 67, 7, 79, 21, 97, 29, 36, 19, 5, 63, 64, 10, 1, 20, 77, 28, 12, 98, 93, 54, 26, 37,
+                73, 60, 88, 96, 82, 35, 2, 86, 66, 46, 9, 52, 45, 87, 22, 44, 25, 75, 4, 3, 69, 14, 32, 68, 92, 7, 79,
+                21, 97, 29, 36, 19, 5, 63, 64, 10, 1, 20 };
+        doubleSelectionSort(nums3);
         System.out.println(Arrays.toString(nums3));
-        int[] nums4 = { 48, 8, 78, 67, 7, 79, 21, 97, 29, 36, 19, 5, 63, 64, 10, 1, 20, 77, 28, 12, 98, 93, 54, 26, 37, 73, 60, 88, 96, 82, 35, 2, 86, 66, 46, 9, 52, 45, 87, 22, 44, 25, 75, 4, 3, 69, 14, 32, 68, 92, 7, 79, 21, 97, 29, 36, 19, 5, 63, 64, 10, 1, 20 };
-        doubleSelectionSort(nums4);
+        int[] nums4 = { 48, 8, 78, 67, 7, 79, 21, 97, 29, 36, 19, 5, 63, 64, 10, 1, 20, 77, 28, 12, 98, 93, 54, 26, 37,
+                73, 60, 88, 96, 82, 35, 2, 86, 66, 46, 9, 52, 45, 87, 22, 44, 25, 75, 4, 3, 69, 14, 32, 68, 92, 7, 79,
+                21, 97, 29, 36, 19, 5, 63, 64, 10, 1, 20 };
+        bubbleSort(nums4);
         System.out.println(Arrays.toString(nums4));
-        int[] nums5 = { 48, 8, 78, 67, 7, 79, 21, 97, 29, 36, 19, 5, 63, 64, 10, 1, 20, 77, 28, 12, 98, 93, 54, 26, 37, 73, 60, 88, 96, 82, 35, 2, 86, 66, 46, 9, 52, 45, 87, 22, 44, 25, 75, 4, 3, 69, 14, 32, 68, 92, 7, 79, 21, 97, 29, 36, 19, 5, 63, 64, 10, 1, 20 };
-        bubbleSort(nums5);
+        int[] nums5 = { 48, 8, 78, 67, 7, 79, 21, 97, 29, 36, 19, 5, 63, 64, 10, 1, 20, 77, 28, 12, 98, 93, 54, 26, 37,
+                73, 60, 88, 96, 82, 35, 2, 86, 66, 46, 9, 52, 45, 87, 22, 44, 25, 75, 4, 3, 69, 14, 32, 68, 92, 7, 79,
+                21, 97, 29, 36, 19, 5, 63, 64, 10, 1, 20 };
+        mergeSort(nums5);
         System.out.println(Arrays.toString(nums5));
     }
 }
